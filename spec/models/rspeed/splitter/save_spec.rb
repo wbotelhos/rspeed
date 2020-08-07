67d3a81b13bab80d26a89d945c9264ad1d51f80c
Washington Botelho <wbotelhos@gmail.com>
2020-08-07 16:57:53 -0300

    spec: now split receives a parsed data

--- spec/models/rspeed/splitter/save_spec.rb
+++ spec/models/rspeed/splitter/save_spec.rb
@@ -7,24 +7,34 @@ RSpec.describe RSpeed::Splitter, '.save' do
 
   let!(:redis) { redis_object }
 
-  before { allow(splitter).to receive(:pipes).and_return 3 }
+  before do
+    allow(splitter).to receive(:pipes).and_return(3)
+
+    redis.lpush 'rspeed', { file: "./spec/0_2_spec.rb", time: '0.2' }.to_json
+    redis.lpush 'rspeed', { file: "./spec/0_3_spec.rb", time: '0.3' }.to_json
+    redis.lpush 'rspeed', { file: "./spec/0_4_spec.rb", time: '0.4' }.to_json
+    redis.lpush 'rspeed', { file: "./spec/0_7_spec.rb", time: '0.7' }.to_json
+    redis.lpush 'rspeed', { file: "./spec/1_1_spec.rb", time: '1.1' }.to_json
+    redis.lpush 'rspeed', { file: "./spec/1_5_spec.rb", time: '1.5' }.to_json
+    redis.lpush 'rspeed', { file: "./spec/2_0_spec.rb", time: '2.0' }.to_json
+  end
 
   it 'saves the data on redis' do
     splitter.save
 
-    expect(redis.keys('*')).to eq %w[rspeed_1 rspeed_2 rspeed_3]
+    expect(redis.keys('*')).to eq %w[rspeed rspeed_1 rspeed_2 rspeed_3]
 
     expect(JSON.parse(redis.get('rspeed_1'), symbolize_names: true)).to eq(
-      files: [{ file: './spec/2_0_spec.rb', time: '2.0' }],
+      files: [{ file: './spec/2_0_spec.rb', time: 2.0 }],
       number: 1,
       total: 2.0
     )
 
     expect(JSON.parse(redis.get('rspeed_2'), symbolize_names: true)).to eq(
       files: [
-        { file: './spec/1_5_spec.rb', time: '1.5' },
-        { file: './spec/0_4_spec.rb', time: '0.4' },
-        { file: './spec/0_2_spec.rb', time: '0.2' }
+        { file: './spec/1_5_spec.rb', time: 1.5 },
+        { file: './spec/0_4_spec.rb', time: 0.4 },
+        { file: './spec/0_2_spec.rb', time: 0.2 }
       ],
 
       number: 2,
@@ -33,9 +43,9 @@ RSpec.describe RSpeed::Splitter, '.save' do
 
     expect(JSON.parse(redis.get('rspeed_3'), symbolize_names: true)).to eq(
       files: [
-        { file: './spec/1_1_spec.rb', time: '1.1' },
-        { file: './spec/0_7_spec.rb', time: '0.7' },
-        { file: './spec/0_3_spec.rb', time: '0.3' }
+        { file: './spec/1_1_spec.rb', time: 1.1 },
+        { file: './spec/0_7_spec.rb', time: 0.7 },
+        { file: './spec/0_3_spec.rb', time: 0.3 }
       ],
 
       number: 3,
--- spec/models/rspeed/splitter/split_spec.rb
+++ spec/models/rspeed/splitter/split_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe RSpeed::Splitter, '.split' do
 
   let!(:data) do
     [
-      { file: './spec/2_0_spec.rb', time: '2.0' }.to_json,
-      { file: './spec/1_5_spec.rb', time: '1.5' }.to_json,
-      { file: './spec/1_1_spec.rb', time: '1.1' }.to_json,
-      { file: './spec/0_7_spec.rb', time: '0.7' }.to_json,
-      { file: './spec/0_4_spec.rb', time: '0.4' }.to_json,
-      { file: './spec/0_3_spec.rb', time: '0.3' }.to_json,
-      { file: './spec/0_2_spec.rb', time: '0.2' }.to_json,
+      { file: './spec/2_0_spec.rb', time: '2.0' },
+      { file: './spec/1_5_spec.rb', time: '1.5' },
+      { file: './spec/1_1_spec.rb', time: '1.1' },
+      { file: './spec/0_7_spec.rb', time: '0.7' },
+      { file: './spec/0_4_spec.rb', time: '0.4' },
+      { file: './spec/0_3_spec.rb', time: '0.3' },
+      { file: './spec/0_2_spec.rb', time: '0.2' },
     ]
   end
 
@@ -22,16 +22,16 @@ RSpec.describe RSpeed::Splitter, '.split' do
   it 'splits the times between the pipes' do
     expect(splitter.split(data)).to eq(
       rspeed_1: {
-        files: [{ file: './spec/2_0_spec.rb', time: '2.0' }],
+        files: [{ file: './spec/2_0_spec.rb', time: 2.0 }],
         number: 1,
         total: 2.0
       },
 
       rspeed_2: {
         files: [
-          { file: './spec/1_5_spec.rb', time: '1.5' },
-          { file: './spec/0_4_spec.rb', time: '0.4' },
-          { file: './spec/0_2_spec.rb', time: '0.2' }
+          { file: './spec/1_5_spec.rb', time: 1.5 },
+          { file: './spec/0_4_spec.rb', time: 0.4 },
+          { file: './spec/0_2_spec.rb', time: 0.2 }
         ],
 
         number: 2,
@@ -40,9 +40,9 @@ RSpec.describe RSpeed::Splitter, '.split' do
 
       rspeed_3: {
         files: [
-          { file: './spec/1_1_spec.rb', time: '1.1' },
-          { file: './spec/0_7_spec.rb', time: '0.7' },
-          { file: './spec/0_3_spec.rb', time: '0.3' }
+          { file: './spec/1_1_spec.rb', time: 1.1 },
+          { file: './spec/0_7_spec.rb', time: 0.7 },
+          { file: './spec/0_3_spec.rb', time: 0.3 }
         ],
 
         number: 3,
