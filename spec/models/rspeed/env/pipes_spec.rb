f436c20f8a28b84f4ed2105f86736f3a1f6f54c1
Washington Botelho <wbotelhos@gmail.com>
2020-12-05 15:46:14 -0300

    up: pipes returns the total as default

--- spec/models/rspeed/env/pipes_spec.rb
+++ spec/models/rspeed/env/pipes_spec.rb
@@ -34,4 +34,20 @@ RSpec.describe RSpeed::Env, '.pipes' do
       end
     end
   end
+
+  context 'when result is not given' do
+    context 'when env is setted' do
+      it 'returns the env value as integer considering result as true' do
+        EnvMock.mock(rspeed_pipes: '2') do
+          expect(described_class.pipes).to be(2)
+        end
+      end
+    end
+
+    context 'when env is not setted' do
+      it 'returns number 1 considering result as true' do
+        expect(described_class.pipes).to be(1)
+      end
+    end
+  end
 end
